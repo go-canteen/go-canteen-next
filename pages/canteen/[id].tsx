1462ae0046dd8d6b653106a62dc981354cbc09df
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-09-26 19:01:56 +0700

    chore: add tailwindcss

---
+++ pages/canteen/[id].tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const CanteenDetailPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Detail Kantin</title>
+      </Head>
+
+      <main className="">
+        <h1 data-testid="welcome-header">
+          Welcome to <a href="#">Kantin Fasilkom</a>
+        </h1>
+
+        <h2>List Merchant</h2>
+      </main>
+    </>
+  );
+};
+
+export default CanteenDetailPage;
