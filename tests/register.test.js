4c16d96ee5487c0c4e528fd384f9ba98c5cbeed0
valentinohp <valentinohp11@gmail.com>
2022-10-09 21:33:16 +0700

    [RED] added test for selecting role

--- tests/register.test.js
+++ tests/register.test.js
@@ -5,7 +5,8 @@ import userEvent from "@testing-library/user-event";
 
 it("allows me to register a new account", () => {
     render(<Form />)
-
+    
+    userEvent.selectOptions(screen.getByTestId("role"), ["merchant"])
     userEvent.type(screen.getByLabelText(/Masukkan nama/i), "John Doe")
     userEvent.type(screen.getByLabelText(/Masukkan e-mail/i), "John@example.com")
     userEvent.type(screen.getByLabelText(/Masukkan kata sandi/i), "password123")
