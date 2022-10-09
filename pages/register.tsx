6bad43a9acbf770bab2c6e91d25e2530cb6247dd
valentinohp <valentinohp11@gmail.com>
2022-10-09 21:34:11 +0700

    [GREEN] implemented role select when registering a new user

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,7 @@
 export default function Register() {
     return (
         <div>
-            <button>Kembali</button>
+            <a href="./">Kembali</a>
             <h1>Daftar ke Canteen</h1>
             <Form />
         </div>
@@ -10,16 +10,25 @@ export default function Register() {
 
 export function Form() {
     return (
-        <form action="#" method="POST">
-            <label htmlFor="name">Masukkan nama Customer/Merchant</label>
-            <input name="name" id="name" type="text" placeholder="Nama Customer/Merchant" required /><br />
-            <label htmlFor="email">Masukkan e-mail kamu</label>
-            <input name="email" id="email" type="text" placeholder="Alamat e-mail" required /><br />
-            <label htmlFor="password1">Masukkan kata sandi kamu</label>
-            <input name="password1" id="password1" type="password" placeholder="Kata sandi" required /><br />
-            <label htmlFor="password2">Ulangi kata sandi kamu sekali lagi</label>
-            <input name="password2" id="password2" type="password" placeholder="Kata sandi" required /><br />
-            <button type="submit">DAFTAR</button>
-        </form>
+        <div>
+            <form action="#" method="POST">
+                <label htmlFor="name">Masukkan nama Customer/Merchant</label>
+                <select name="role" id="role" data-testid="role">
+                    <option value="customer">Customer</option>
+                    <option value="merchant">Merchant</option>
+                </select>
+                <input name="name" id="name" type="text" placeholder="Nama Customer/Merchant" required /><br />
+                <label htmlFor="email">Masukkan e-mail kamu</label>
+                <input name="email" id="email" type="text" placeholder="Alamat e-mail" required /><br />
+                <label htmlFor="password1">Masukkan kata sandi kamu</label>
+                <input name="password1" id="password1" type="password" placeholder="Kata sandi" required /><br />
+                <label htmlFor="password2">Ulangi kata sandi kamu sekali lagi</label>
+                <input name="password2" id="password2" type="password" placeholder="Kata sandi" required /><br />
+                <button type="submit">DAFTAR</button>
+            </form>
+            <div>
+                Sudah punya akun? <a href="/login">Masuk disini</a>
+            </div>
+        </div>
     );
 }
