448bf82d0c4eb1196dc6dd40f577747c07db591b
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-10-13 17:24:36 +0700

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
+  collectCoverageFrom: [
+    "components/**/*.{js,jsx,ts,tsx}",
+    "pages/**/*.{js,jsx,ts,tsx}",
+    "lib/**/*.{js,jsx,ts,tsx}",
+  ],
 };
 module.exports = createJestConfig(customJestConfig);
