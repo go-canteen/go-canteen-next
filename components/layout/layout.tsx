b2ec94256eb6e2f9ad80d3b5beab6cb4d461c355
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-10-31 18:21:20 +0700

    feat: update layout component

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -10,13 +10,15 @@ import {
   ArrowPathIcon as ArrowPathIconSolid,
   UserCircleIcon as UserCircleIconSolid,
 } from "@heroicons/react/24/solid";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { NavItem } from "./nav-item";
 
 export const Layout = ({ children }) => {
   return (
-    <div className="max-w-screen-sm mx-auto bg-white">
+    <div className="max-w-xl mx-auto bg-white">
       <div className="container min-h-screen pb-16">{children}</div>
-      <div className="w-full max-w-screen-sm flex bg-green-300 h-16 justify-between py-4 px-8 fixed bottom-0">
+      <div className="w-full max-w-xl flex bg-white border-t-2 h-16 justify-between py-4 px-8 fixed bottom-0">
         {NAV_ITEMS.map((item, i) => (
           <NavItem key={i} {...item} />
         ))}
@@ -27,25 +29,25 @@ export const Layout = ({ children }) => {
 
 const NAV_ITEMS = [
   {
-    label: "Home",
+    label: "Toko",
     href: "/",
     icon: <HomeIcon className="h-8 w-8" />,
     activeIcon: <HomeIconSolid className="h-8 w-8" />,
   },
   {
-    label: "Order",
+    label: "Pesanan",
     href: "/order",
     icon: <ClipboardDocumentIcon className="h-8 w-8" />,
     activeIcon: <ClipboardDocumentIconSolid className="h-8 w-8" />,
   },
   {
-    label: "History",
+    label: "Riwayat",
     href: "/history",
     icon: <ArrowPathIcon className="h-8 w-8" />,
     activeIcon: <ArrowPathIconSolid className="h-8 w-8" />,
   },
   {
-    label: "Account",
+    label: "Akun",
     href: "/account",
     icon: <UserCircleIcon className="h-8 w-8" />,
     activeIcon: <UserCircleIconSolid className="h-8 w-8" />,
