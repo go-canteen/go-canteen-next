e360c81e68923c11377bd021beb94cc3e8b08267
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-11-14 18:24:22 +0700

    feat: integrate login page with login api

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import axios, { AxiosError } from "axios";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -12,58 +13,34 @@ export const authOptions = {
       },
       async authorize(credentials, req) {
         // TODO: uncomment and modify lines below when the backend api is ready, we are using dummy user for now
-        //
-        // const data = await axios
-        //   .post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/dj-rest-auth/login/`, {
-        //     email: credentials?.email,
-        //     password: credentials?.password,
-        //   })
-        //   .then((res) => {
-        //     return res.data;
-        //   })
-        //   .catch((err: AxiosError) => {
-        //     return err.response?.data;
-        //   });
-        //
-        // if (!data?.key) throw new Error(JSON.stringify(data));
-        //
-        // const { data: userDetail } = await axios.get(
-        //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/me`,
-        //   {
-        //     headers: {
-        //       Authorization: `Token ${data.key}`,
-        //     },
-        //   }
-        // );
-        //
-        // return {
-        //   ...userDetail,
-        //   key: data.key,
-        // };
 
-        const email = credentials?.email;
+        const data = await axios
+          .post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login/`, {
+            email: credentials?.email,
+            password: credentials?.password,
+          })
+          .then((res) => {
+            return res.data;
+          })
+          .catch((err: AxiosError) => {
+            return err.response?.data;
+          });
 
-        if (email === "dummyuser@gocanteen.com") {
-          return {
-            id: "2fb335bc-7139-49d8-acbe-1be337156361",
-            email: "dummyuser@gocanteen.com",
-            role: "customer",
-            key: "9a114ec8-0c98-4ffd-8a02-2fa288737774",
-            display_name: "Dadang Korneto",
-          };
-        }
+        if (!data?.key) throw new Error(JSON.stringify(data));
 
-        if (email === "dummyseller@gocanteen.com") {
-          return {
-            id: "2fb335bc-7139-49d8-acbe-1be337156362",
-            email: "dummyseller@gocanteen.com",
-            role: "seller",
-            key: "9a114ec8-0c98-4ffd-8a02-2fa288737776",
-            display_name: "Tukang Sate",
-          };
-        }
+        const { data: userDetail } = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/me`,
+          {
+            headers: {
+              Authorization: `Token ${data.key}`,
+            },
+          }
+        );
 
-        throw new Error(JSON.stringify("Login gagal"));
+        return {
+          ...userDetail,
+          key: data.key,
+        };
       },
     }),
   ],
